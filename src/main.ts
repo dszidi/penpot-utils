0079fe7fa8d0190632f480ee144a14098ef70d0a
Damian Szidiropulosz <damianos@knoxbug.org>
2025-06-05 18:35:07 -0400

    Basic functionality. Select all slots, by slot range or by slot list

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,156 @@
 import "./style.css";
+import { Notification } from './plugin.ts';
 
 // get the current theme from the URL
 const searchParams = new URLSearchParams(window.location.search);
 document.body.dataset.theme = searchParams.get("theme") ?? "light";
 
-document.querySelector("[data-handler='create-text']")?.addEventListener("click", () => {
+document.querySelector("[data-handler='current-selection']")?.addEventListener("click", () => {
   // send message to plugin.ts
-  parent.postMessage("create-text", "*");
+  parent.postMessage({ message: "current-selection" }, "*");
 });
 
-// Listen plugin.ts messages
-window.addEventListener("message", (event) => {
-  if (event.data.source === "penpot") {
-    document.body.dataset.theme = event.data.theme;
+document.querySelector("[data-handler='list-enclosures']")?.addEventListener("click", () => {
+  // send message to plugin.ts
+  parent.postMessage({ message: "list-enclosure-options" }, "*");
+});
+
+document.querySelector("[data-handler='select-all-drivetray-handles']")?.addEventListener("click", () => {
+  // send message to plugin.ts
+  parent.postMessage({ message: "select-all-drivetray-handles" }, "*");
+});
+
+
+// ENCLOSURE OPTIONS
+const enclosureSelect = document.getElementById("enclosure-select") as HTMLSelectElement;
+
+enclosureSelect.addEventListener("change", () => {
+  const selected = enclosureSelect.value;
+  if (selected) {
+    const notification = {
+      message: 'select-enclosure',
+      data: selected,
+    }
+
+    parent.postMessage(notification, "*");
   }
 });
+
+function updateSelectOptions(names: string[]) {
+  enclosureSelect.innerHTML = '<option value="">Enclosure Instance</option>';
+
+  names.forEach((name) => {
+    const option = document.createElement("option");
+    option.value = name;
+    option.textContent = name;
+    enclosureSelect.appendChild(option);
+  });
+}
+
+
+// SLOT RANGE
+const slotRange = document.getElementById("slot-range") as HTMLSelectElement;
+
+slotRange.addEventListener("input", () => {
+  let payload: string[] | null = null;
+  const value = slotRange.value;
+  const isRange = value.includes('-');
+  const isList = value.includes(',');
+  let isValid = false;
+
+  if (isRange) {
+    isValid = isValidRange(value);
+  } else if (isList) {
+    isValid = isValidList(value);
+  }
+    
+  if (value && isValid) {
+    // Send a list even if it is a range
+    payload = isRange ? rangeToList(value) : value.split(',').filter(Boolean);
+  } 
+
+  const notification = {
+    message: 'set-range',
+    data: payload,
+  }
+
+  parent.postMessage(notification, "*");
+});
+
+function rangeToList (range: string): string[] {
+  let list: string[] = [];
+  
+  // Assume everything is already validated
+  // Sort based on numeric value
+  const sanitized = range.split('-').map((slot) => Number(slot)).sort().map((slot) => slot.toString());
+
+  for (let i = Number(sanitized[0]); i <= Number(sanitized[1]); i++) {
+    list.push(i.toString());
+  }
+
+  return list;
+}
+
+function isValidRange (value: string): boolean {
+  let output = false;
+  let isRange = value.includes('-');
+
+  if (!isRange || value.split('-').filter(Boolean).length !== 2) {
+    return output;
+  }
+  
+  const tally = 0;
+  const reducedValue = value.split('-').reduce(
+    (accumulator, currentValue) => accumulator + Number(currentValue),
+    tally,
+  );
+
+  output = isRange && reducedValue > 2; // smallest slot combination is 1 and 2
+
+  return output;
+}
+
+function isValidList (value: string): boolean {
+  let output = false;
+  let isList = value.includes(',');
+  const split = value.split(',');
+
+  const deduped = [...new Set(split)];
+
+  if (!isList || deduped.length < value.split(',').length) {
+    return output;
+  }
+  
+  const tally = 0;
+  const reducedValue = value.split(',').reduce(
+    (accumulator, currentValue) => accumulator + Number(currentValue),
+    tally,
+  );
+
+  output = isList && reducedValue > 2; // smallest slot combination is 1 and 2
+
+  return output;
+}
+
+
+// PLUGIN EVENTS
+window.addEventListener("message", (event) => {
+  const notification: Notification = event.data;
+  switch (notification.message) {
+    case "enclosure-options": 
+      updateSelectOptions(notification.data as string[]);
+      break;
+  default:
+    console.warn(notification);
+  }  
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  init();
+});
+
+function init() {
+  // Your startup logic here
+  parent.postMessage({ message: "list-enclosure-options" }, "*");  
+}
+
--- src/plugin.ts
+++ src/plugin.ts
@@ -1,15 +1,67 @@
-penpot.ui.open("PenPot Utils", `?theme=${penpot.theme}`);
+import { Page, Shape } from 'plugin-types';
 
-penpot.ui.onMessage<string>((message) => {
-  if (message === "create-text") {
-    const text = penpot.createText("Hello world!");
+export interface Notification {
+  message: string;
+  data: unknown | null;
+}
 
-    if (text) {
-      text.x = penpot.viewport.center.x;
-      text.y = penpot.viewport.center.y;
+export interface EnclosureConfig {
+  selectedEnclosure: string | null; // Shape | null;
+  slotList: string[];
+}
 
-      penpot.selection = [text];
-    }
+let enclosureConfig: EnclosureConfig = {
+  selectedEnclosure: null,
+  slotList: [],
+}
+
+penpot.ui.open("TrueNAS Utils", `?theme=${penpot.theme}`);
+
+penpot.ui.onMessage<Notification>((notification: Notification) => {
+  switch (notification.message) {
+    case 'current-selection':
+      break;
+    case 'list-enclosure-options':
+      const enclosures: Shape[] = listEnclosures();
+      penpot.ui.sendMessage({
+        message: "enclosure-options",
+        data: enclosures.map((e: Shape) => e.name),
+      });
+      break;
+    case 'select-enclosure':      
+      enclosureConfig.selectedEnclosure = notification.data as string;
+      break;
+    case 'set-range':
+      enclosureConfig.slotList = notification.data ? notification.data as string[] : [];
+      break;
+    case 'select-all-drivetray-handles':    
+      const currentPage: Page | null = penpot.currentPage;
+      let drivetrays: Shape[] | undefined = currentPage?.findShapes({
+        nameLike: 'handle-', // Components seem to be registered as boards??
+      }).filter((handle) => { // Filter by enclosure instance name
+        const instanceName = handle.componentHead()?.parent?.parent?.parent?.name;        
+        return instanceName == enclosureConfig.selectedEnclosure;
+      });
+
+      if (enclosureConfig.slotList.length) {
+        drivetrays = drivetrays.filter((handle) => { // filter by slot list
+        const traySlot = handle.componentHead()?.name.split(' ')[1]; // component name is 'drivetray-slot <slot number>'
+        const configSlot = enclosureConfig.slotList.filter((slot) => Number(traySlot) == Number(slot));      
+        return Number(traySlot) == Number(configSlot);
+        });
+      }
+
+      if (drivetrays) {
+        penpot.selection = drivetrays;
+      }
+      break;
+
+    case 'selection-changed':
+      console.log('selection changed');
+      break;
+
+    default:
+      console.warn('Unknown message type:', notification.message);
   }
 });
 
@@ -21,3 +73,20 @@ penpot.on("themechange", (theme) => {
     theme,
   });
 });
+
+
+function listEnclosures(): Shape[] {
+  const currentPage: Page | null = penpot.currentPage;
+
+  const enclosures: Shape[] = currentPage.findShapes({
+    type: 'board', // Components seem to be registered as boards??
+  }).filter((enclosure) => {
+    const isInstance = (enclosure.isComponentInstance() || enclosure.component());
+    const isEnclosureComponent = enclosure.component()?.name.includes('enclosure')
+    const isNotSubComponent = !enclosure.name.includes('drivetray') && !enclosure.name.includes('chassis');
+
+    return ( isInstance && isEnclosureComponent && isNotSubComponent);
+  });
+
+  return enclosures;
+}
